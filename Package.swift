cf8cc57ad9a1f701bc428a4c6e3597db8d6ff3c2
Zaheer <zaheer-dvt@paystack.com>
2023-02-15 20:48:49 +0200

    Fixed package names

--- Package.swift
+++ Package.swift
@@ -2,18 +2,18 @@
 import PackageDescription
 
 let package = Package(
-  name: "fantastic-chainsaw",
+  name: "chainsaw",
   platforms: [
     .macOS(.v10_15), .iOS(.v14)
   ],
   products: [
     .library(
-      name: "fantastic-chainsaw",
-      targets: ["fantastic-chainsaw"]),
+      name: "chainsaw",
+      targets: ["chainsaw"]),
   ],
   targets: [
     .binaryTarget(
-      name: "fantastic-chainsaw",
+      name: "chainsaw",
       url: "https://github.com/Zamo22/fantastic-chainsaw/releases/download/0.0.6/chainsaw.xcframework.zip",
       checksum: "e1d03c3a65a975390300087b19be95ca9b6fbc03b809f9f0a2bee9ae1bf40d76"
     )
