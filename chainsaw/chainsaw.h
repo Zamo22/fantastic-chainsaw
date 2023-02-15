f1b2ee29cc8f50ca860a15d31cc2efef802ad44c
Zaheer <zaheer-dvt@paystack.com>
2023-02-15 20:39:09 +0200

    Changed name of framework

---
+++ chainsaw/chainsaw.h
@@ -0,0 +1,18 @@
+//
+//  chainsaw.h
+//  chainsaw
+//
+//  Created by Zaheer Moola on 2023/02/15.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for chainsaw.
+FOUNDATION_EXPORT double chainsawVersionNumber;
+
+//! Project version string for chainsaw.
+FOUNDATION_EXPORT const unsigned char chainsawVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <chainsaw/PublicHeader.h>
+
+
