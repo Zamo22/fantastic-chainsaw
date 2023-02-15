666fe46d1da62dc26c78eb90c18f73df87ca1db9
Zamo22 <zmoola22@gmail.com>
2023-02-15 18:58:43 +0000

    Updating Package file for release

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "chainsaw",
-      url: "https://github.com/Zamo22/fantastic-chainsaw/releases/download/0.0.6/chainsaw.xcframework.zip",
-      checksum: "e1d03c3a65a975390300087b19be95ca9b6fbc03b809f9f0a2bee9ae1bf40d76"
+      url: "https://github.com/Zamo22/fantastic-chainsaw/releases/download/0.0.7/chainsaw.xcframework.zip",
+      checksum: "17e8e8b58984eb9a9495ad1a356cd3c1bc9dbff3c82866aced112d117d7f05dd"
     )
   ]
 )
