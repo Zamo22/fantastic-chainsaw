166c094bc3bafe0db1a98d9452ca0d5964794456
Zaheer <zaheer-dvt@paystack.com>
2023-02-15 19:24:21 +0200

    Fixed typos in package file

--- Package.swift
+++ Package.swift
@@ -2,18 +2,18 @@
 import PackageDescription
 
 let package = Package(
-  name: "fantastic_chainsaw",
+  name: "fantastic-chainsaw",
   platforms: [
     .macOS(.v10_15), .iOS(.v14)
   ],
   products: [
     .library(
-      name: "fantastic_chainsaw",
-      targets: ["fantastic_chainsaw"]),
+      name: "fantastic-chainsaw",
+      targets: ["fantastic-chainsaw"]),
   ],
   targets: [
     .binaryTarget(
-      name: "fantastic_chainsaw",
+      name: "fantastic-chainsaw",
       url: "https://github.com/Zamo22/fantastic-chainsaw/releases/download/0.0.3/fantastic-chainsaw.xcframework.zip",
       checksum: "b0c6e356cf1b68db8e35c43ab98218e59f2d91583313ba03fb0f62f1a57c21c5"
     )
